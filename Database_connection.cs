c21e4502741cdb4f02009e547dc30177dc6f2330
Code-Solution-2016 <owenme10000@gmail.com>
2025-03-20 14:57:09 +0200

    Update Database_connection.cs

--- Database_connection.cs
+++ Database_connection.cs
@@ -10,8 +10,7 @@ namespace appr_poe_part_2_owen_muhluri_ndimande_GIFT_OF_GIVERS
         public string Database_String_connection()
         {
             //temp variable
-            string connection = "Server=tcp:code-solutions.database.windows.net,1433;Initial Catalog=rosebank_databases;Persist Security Info=False;User ID=code-solution;Password=RosebankFree@1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;";
-
+            string connection = "";
             //then return to connect
             return connection;
         }
